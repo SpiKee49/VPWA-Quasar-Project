96d7146ff1d8c030824b10b742d6c6e22430c919
Sisa1265 <xkuchtovas@stuba.sk>
2023-10-22 12:10:08 +0200

    create log in page

---
+++ src/pages/login/LogInPage.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="row window-height window-width justify-center items-center">
+        <q-card class="q-pa-md" style="width:25%">
+            <q-card-section>
+                <div>
+                    <h2 class="text-h2 text-center text-uppercase q-my-none text-weight-regular">Log in</h2>
+                </div>
+            </q-card-section>
+            <q-card-section>
+                <q-form class="q-gutter-md text-white">
+                    <q-input color="info" label="Username"></q-input>
+                    <q-input color="info" label="Password" type="password"></q-input>
+                    <div>
+                        <q-btn class="full-width" color="info" label="Log in" rounded></q-btn>
+                        <div class="row justify-around q-mt-md" >
+                            <router-link class="text-white" to="/login">Zabudnuté heslo?</router-link>
+                            <router-link class="text-white" to="/login">Vytvoriť konto</router-link>
+                        </div>
+                    </div>   
+                </q-form>
+            </q-card-section>
+        </q-card>
+    </div>    
+</template>
+
+
+<script setup lang="ts">
+
+</script>
+
