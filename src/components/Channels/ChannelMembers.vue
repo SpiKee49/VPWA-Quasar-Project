b05eadcd0a727b6622ed146b0505a46c3c58df41
René Bukovina <r.bukovina@gmail.com>
2023-10-23 13:11:00 +0200

    fguicgh

--- src/components/Channels/ChannelMembers.vue
+++ src/components/Channels/ChannelMembers.vue
@@ -1,15 +1,40 @@
 <template>
     <p class="q-pa-sm bg-info q-ma-none text-bold">Channel members</p>
     <q-list>
-        <q-item v-for="item in [1,2,3,4,5,6,7]" :key="item" clickable v-ripple>
+        <q-item
+            v-for="item in [1, 2, 3, 4, 5, 6, 7]"
+            :key="item"
+            clickable
+            v-ripple
+        >
             <q-item-section avatar>
                 <q-icon color="white" size="sm" name="fa-solid fa-user" />
             </q-item-section>
-            <q-item-section>@username{{item}}</q-item-section>
+            <q-item-section>@username{{ item }}</q-item-section>
+            <q-menu fit transition-show="fade" transition-hide="fade">
+                <q-list flat style="min-width: 200px" class="bg-darker">
+                    <q-item clickable v-close-popup class="text-red">
+                        <q-item-section avatar>
+                            <q-icon color="red" name="fa-solid fa-door-open" />
+                        </q-item-section>
+                        <q-item-section> Kick </q-item-section>
+                    </q-item>
+                    <q-item clickable v-close-popup class="text-red">
+                        <q-item-section avatar>
+                            <q-icon color="red" name="fa-solid fa-ban" />
+                        </q-item-section>
+                        <q-item-section> Ban </q-item-section>
+                    </q-item>
+                </q-list>
+            </q-menu>
         </q-item>
     </q-list>
 </template>
 
-<script setup lang="ts">
+<script setup lang="ts"></script>
 
-</script>
+<style scoped lang="scss">
+.menu-item-icon {
+    display: inline-block;
+}
+</style>
