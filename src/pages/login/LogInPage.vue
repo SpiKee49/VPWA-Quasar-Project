e126975c0bf4510df14511e8d40526b58466788d
Sisa1265 <xkuchtovas@stuba.sk>
2023-10-24 10:14:34 +0200

    add registration page

--- src/pages/login/LogInPage.vue
+++ src/pages/login/LogInPage.vue
@@ -3,18 +3,19 @@
         <q-card class="q-pa-md" style="width:25%">
             <q-card-section>
                 <div>
-                    <h2 class="text-h2 text-center text-uppercase q-my-none text-weight-regular">Log in</h2>
+                    <h2 class="text-h2 text-center text-uppercase q-my-none text-weight-regular">{{isLogin ? 'Log in':'Register'}}</h2>
                 </div>
             </q-card-section>
             <q-card-section>
                 <q-form class="q-gutter-md text-white">
                     <q-input color="info" label="Username"></q-input>
                     <q-input color="info" label="Password" type="password"></q-input>
+                    <q-input v-if="!isLogin" color="info" label="Repeat password" type="password"></q-input>
                     <div>
-                        <q-btn class="full-width" color="info" label="Log in" rounded></q-btn>
+                        <q-btn class="full-width" to="/" color="info" :label="isLogin ? 'Log in':'Register'" rounded></q-btn>
                         <div class="row justify-around q-mt-md" >
-                            <router-link class="text-white" to="/login">Zabudnuté heslo?</router-link>
-                            <router-link class="text-white" to="/login">Vytvoriť konto</router-link>
+                            <router-link class="text-white" to="/login">Forgot password?</router-link>
+                            <p class="text-white" style="text-decoration: underline; cursor: pointer;" @click="isLogin = !isLogin">{{isLogin ? 'Register':'Log in'}}</p>
                         </div>
                     </div>   
                 </q-form>
@@ -25,6 +26,13 @@
 
 
 <script setup lang="ts">
+import { ref } from 'vue';
+
+
+    const isLogin = ref(true)
+    
+    
+
 
 </script>
 
